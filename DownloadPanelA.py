4dd26d4ad9a1f1ab29be9605c398df96280d34d0
RPChinhara <rpchinhara@gmail.com>
2022-06-27 18:44:43 +0530

    deleted or moved

--- DownloadPanelA.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-with open('panelA.txt') as f:
-    lines = f.read().splitlines()
-
-for entry in lines:
-    subprocess.call(["./pfda", "download", "--file-id", entry, "--output", "/mnt/d/FDA/"])
--- download_panelA.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-filename='panelA.txt'
-while read p; do 
-    ./pfda download --file-id $p --output /mnt/d/'Ashoka Internship'/FDA  
-done < "$filename"
